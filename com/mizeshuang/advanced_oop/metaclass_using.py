79f01784324bb4d655545126d4313dbe31327058
mizeshuang <329804334@qq.com>
2017-06-20 14:13:21 +0800

    add metaclass_using

--- com/mizeshuang/advanced_oop/metaclass_using.py
+++ com/mizeshuang/advanced_oop/metaclass_using.py
@@ -71,10 +71,46 @@ class ModelMetaclass(type):
 	def __new__(cls, name, bases,attrs):
 		if name == 'Model':
 			return type.__new__(cls, name, bases, attrs)
+		print('Founding Model: %s' % name)
+		mappings = dict()
+		for k, v in attrs.items():
+			if isinstance(v, Field):
+				print('Found mapping: %s ==> %s' % (k, v))
+				mappings[k] = v
+		for k in mappings.keys():
+			attrs.pop(k)
+		attrs['__mappings__'] = mappings # 保存属性和列的映射关系
+		attrs['__table__'] = name # 保存表明和类名一样
+		return type.__new__(cls, name, bases, attrs)
 class Model(dict, metaclass = ModelMetaclass):
-	pass
+	def __init__(self, **kw):
+		super(Model, self).__init__(**kw)
+	def __getattr__(self,key):
+		try:
+			return self[key]
+		except KeyError as e:
+			raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+		else:
+			pass
+		finally:
+			pass
+	def __setattr__(self, key, value):
+		self[key] = value
+	def save(self):
+		fields = []
+		params = []
+		args = []
+		for k, v in self.__mappings__.items():
+			fields.append(v.name)
+			params.append('?')
+			args.append(getattr(self, k, None))
+		sql = '	insert into %s (%s) values (%s)' % (self.__table__, ','.join(fields), ','.join(params))
+		print('SQL: %s' % sql)
+		print('ARGS: %s' % str(args))
 class User(Model):
 	id = IntegerField('id')
 	name = StringField('name')
 	email = StringField('email')
 	password = StringField('password')
+user = User(id = 12345, name = 'milittle', email = '4356@org.com', password = '123456')
+user.save()
